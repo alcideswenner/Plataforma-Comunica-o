83de1e941968e38b264eeb67b9aa420b326ae69d
Alcides Wenner <alcideswenner.jus@gmail.com>
2021-09-25 20:48:01 -0300

    Project Created

---
+++ backend/src/main/java/com/alcideswenner/plataforma_comunicacao/PlataformaComunicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.alcideswenner.plataforma_comunicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaComunicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaComunicacaoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alcideswenner/plataforma_comunicacao/PlataformaComunicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcideswenner.plataforma_comunicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaComunicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
